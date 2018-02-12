e12374580358d3c04cfb21109439da2fd00777bc
Bradley Meyers <bradley.meyers1993@gmail.com>
2018-02-12 16:19:49 +0800

    Updated Galaxy build commands to work with new system (also works on Zeus)

--- recombine/build_galaxy.sh
+++ recombine/build_galaxy.sh
@@ -1,2 +1,8 @@
-g++ -O3 -ftree-loop-distribution -funroll-all-loops -fdata-sections -fstack-protector -I${CFITSIO_ROOT}/include recombine.cpp main.cpp -L${CFITSIO_ROOT}/lib -lcfitsio -o recombine 
-cp recombine /group/mwaops/stremblay/CODE/bin/
+CXX=`which g++`
+CXX_FLAGS="-O3 -ftree-loop-distribution -funroll-all-loops -fdata-sections -fstack-protector"
+${CXX} -c ${CXX_FLAGS} -I${CFITSIO_ROOT}/include main.cpp -o main.o
+${CXX} -c ${CXX_FLAGS} -I${CFITSIO_ROOT}/include recombine.cpp -o recombine.o
+${CXX} ${CXX_FLAGS} main.o recombine.o -o recombine -L${CFITSIO_ROOT}/lib -L/usr/lib64 -lcfitio -lcurl
+
+#g++ -O3 -ftree-loop-distribution -funroll-all-loops -fdata-sections -fstack-protector -I${CFITSIO_ROOT}/include recombine.cpp main.cpp -L${CFITSIO_ROOT}/lib -lcfitsio -o recombine 
+#cp recombine /group/mwaops/stremblay/CODE/bin/
